534b1145becf8ede58ab23ba95db51b2e0dd0438
iman <iman.emadiie@gmail.com>
2024-12-07 16:43:01 +0330

    Header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Home Page</h1>
+      <section className="pink_container">
+        <h1 className="heading">Pitch your startup <br/> Connect with entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote On Pitches, And Get Noticed in Virtual Competations
+        </p>
+      </section>
     </>
   )
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 const Navbar = async () => {
     const session=await auth()
     return (
-        <header className='px-5 py-3 bg-neutral-900 shadow-sm font-work-sans'>
+        <header className='px-5 py-3 bg-white-100 shadow-sm font-work-sans'>
             <nav className='flex justify-between items-center'>
                 <Link href="/" >
                     <Image src="/logo.svg" alt='logo' width={25} height={25}/>
